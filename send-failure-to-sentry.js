e2889b5e3961517d40dce0003c9a5cdb731a7a16
Sarah Bird <birdsarah@mozilla.com>
2023-04-05 18:21:30 -0500

    Add url of allure report to sentry message

--- send-failure-to-sentry.js
+++ send-failure-to-sentry.js
@@ -5,4 +5,4 @@ Sentry.init({
   dsn: env.SENTRY_DSN,
 });
 
-Sentry.captureMessage("Something went wrong.");
+Sentry.captureMessage("Something went wrong. Please review https://mozilla.github.io/guardian-e2e/");

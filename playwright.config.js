688225c1806cc2c41a48e3072e0c79343c3549b4
Jeremy Swinarton <jswinarton@mozilla.com>
2023-10-23 15:08:24 -0400

    npm update and config upgrades

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ const config = {
     toMatchSnapshot: { maxDiffPixels: 100 }
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  // forbidOnly: !!process.env.CI,
+  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
